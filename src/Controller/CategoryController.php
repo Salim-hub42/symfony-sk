4d3a928a47eb729dfcd9ccbddbc2e0c8bee01248
salim42 <salim.khalfoun@yahoo.fr>
2026-02-02 16:47:02 +0100

    categoryForm

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Category;
+use App\Form\CategoryType;
+
+final class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'app_category')]
+    public function index(): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController de Monsieur khalfoun',
+        ]);
+    }
+
+    #[Route('/category/add', name: 'app_add_category')]
+    public function addCategory(Request $request, EntityManagerInterface $em): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($category);
+            $em->flush();
+            return $this->redirectToRoute('app_category');
+        }
+        return $this->render('category/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
